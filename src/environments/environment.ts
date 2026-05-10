f08a550e52c2bca965120f80aaf7a474363e0183
sasidharan <sasidharan.itprof@gmail.com>
2026-05-11 01:14:36 +0530

    Updated QA urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,13 @@
 
 export const environment = {
   production: false,
+  // apiUrl: 'https://api.ggera.com',
+  // stripePublishKey: 'pk_live_51LeuIfKoXhrXa7bInyggdsBWLztCkaGx74UdsGUrv7RZ1XcPeGUcpjFv6EjJAP18eCgaQyZVk3xmsw0Wp3PTmZ8D00zpJWeiED',
   // apiUrl: 'http://localhost:3001',
-  webUrl: 'https://ggera.com',
   apiUrl: 'https://qa-api.ggera.com',
   stripePublishKey: 'pk_test_51LvGX0SBqo9BCwNOoomZ3V3ke1X8qudxAjDHEV3cfI8sCKbEtm84jcEjrhGkFX3Jm0tR2nRObMLzxAXLaVcmy4F300EmKoIiXM',
-  clientId: '377772772908-lbmj70l12h988v9dk4gb0vknhd5nvj2a.apps.googleusercontent.com'
+  clientId: '377772772908-lbmj70l12h988v9dk4gb0vknhd5nvj2a.apps.googleusercontent.com',
+  webUrl: 'https://qa.ggera.com',
 };
 
 /*
